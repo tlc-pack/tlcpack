60f655cee48163979aaf83c9930bc61c17ac2358
Leandro Nunes <leandro.nunes@arm.com>
2021-05-21 11:42:47 +0100

    Enable USE_MICRO on tlcpack build.
     * Currently we don't have utvm enabled by default in
       a regular TVM build. This PR adds USE_MICRO ON so that
       the package contains utvm.

--- wheel/build_wheel_manylinux.sh
+++ wheel/build_wheel_manylinux.sh
@@ -65,6 +65,7 @@ echo set\(USE_SORT ON\) >> config.cmake
 echo set\(USE_GRAPH_RUNTIME ON\) >> config.cmake
 echo set\(USE_ETHOSN /opt/arm/ethosn-driver\) >> config.cmake
 echo set\(USE_ARM_COMPUTE_LIB /opt/arm/acl\) >> config.cmake
+echo set\(USE_MICRO ON\) >> config.cmake
 if [[ ${CUDA} != "none" ]]; then
     echo set\(USE_CUDA ON\) >> config.cmake
     echo set\(USE_CUBLAS ON\) >> config.cmake
