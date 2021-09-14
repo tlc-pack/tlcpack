d906244e4385cf6fb4050ae57b69aa79ccd5a990
Leandro Nunes <leandro.nunes@arm.com>
2021-09-14 11:56:03 +0100

    Set USE_MICRO_STANDALONE_RUNTIME ON in the Linux wheel builds.

--- wheel/build_wheel_manylinux.sh
+++ wheel/build_wheel_manylinux.sh
@@ -66,6 +66,7 @@ echo set\(USE_GRAPH_RUNTIME ON\) >> config.cmake
 echo set\(USE_ETHOSN /opt/arm/ethosn-driver\) >> config.cmake
 echo set\(USE_ARM_COMPUTE_LIB /opt/arm/acl\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
+echo set\(USE_MICRO_STANDALONE_RUNTIME ON\) >> config.cmake
 if [[ ${CUDA} != "none" ]]; then
     echo set\(USE_CUDA ON\) >> config.cmake
     echo set\(USE_CUBLAS ON\) >> config.cmake
