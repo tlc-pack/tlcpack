a1b254c10363424fca89a2b145c77522db178eb3
Leandro Nunes <leandro.nunes@arm.com>
2022-07-29 15:03:01 +0100

    Add support for tlcpack AArch64 Python packages
    Add support to generate native AArch64 packages with TVM.
    It was tested to generate cpu targeted wheel packages using
    manylinux2014_aarch64.

---
+++ docker/install/centos_install_core_aarch64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+# install libraries for building c++ core on ubuntu
+yum install -y wget xz python3-pip
+
+# install multibuild utils
+git clone https://github.com/matthew-brett/multibuild.git && cd multibuild && \
+    git checkout 9e2349833e994cb829b77cc08f1aacc6ab6d2458
+
+# install argparse
+pip3 install argparse
