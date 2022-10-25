372589b5bcf355c75c70d7d9b8c4665a168b6e98
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-10-25 08:54:45 +0200

    [CI] [Conda] Enable automated CUDA 11.6 builds

--- common/sync_package.py
+++ common/sync_package.py
@@ -156,7 +156,7 @@ def main():
     parser.add_argument("--cuda",
                         type=str,
                         default="none",
-                        choices=["none", "10.2", "11.1", "11.3"],
+                        choices=["none", "10.2", "11.1", "11.3", "11.6"],
                         help="CUDA version to be linked to the resultant binaries,"
                              "or none, to disable CUDA. Defaults to none.")
     parser.add_argument("--package-name",
