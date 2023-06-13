6bf79b9996daa00c8c64da23679248134417170e
Leandro Nunes <leandro.nunes@arm.com>
2023-06-13 21:48:55 +0100

    [LLVM] Update LLVM from 10.0.x to 15.0.x
    Align the LLVM version with main TVM repository, so that
    packages contain the same level of functionality as building
    from source.
    
    Updates validated in both CPU images.

--- docker/install/centos_build_llvm.sh
+++ docker/install/centos_build_llvm.sh
@@ -17,16 +17,20 @@ detect_llvm_version() {
 
 LLVM_VERSION=$(detect_llvm_version)
 echo ${LLVM_VERSION}
-curl -sL https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz -o llvm-${LLVM_VERSION}.src.tar.xz
-unxz llvm-${LLVM_VERSION}.src.tar.xz
-tar xf llvm-${LLVM_VERSION}.src.tar
-pushd llvm-${LLVM_VERSION}.src
+# LLVM
+curl -sL https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-project-${LLVM_VERSION}.src.tar.xz -o llvm-project-${LLVM_VERSION}.src.tar.xz
+unxz llvm-project-${LLVM_VERSION}.src.tar.xz
+tar xf llvm-project-${LLVM_VERSION}.src.tar
+pushd llvm-project-${LLVM_VERSION}.src
+
+pushd llvm
 mkdir build
 pushd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86" \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
     -DLLVM_INCLUDE_DOCS=OFF \
     -DLLVM_INCLUDE_EXAMPLES=OFF \
     -DLLVM_INCLUDE_TESTS=OFF \
@@ -36,39 +40,39 @@ cmake \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_ENABLE_OCAMLDOC=OFF \
     -DLLVM_USE_INTEL_JITEVENTS=ON \
-    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
-    -DPYTHON_EXECUTABLE="$(cpython_path 3.7)/bin/python" \
+    -DPYTHON_EXECUTABLE="$(cpython_path 3.8)/bin/python" \
     -GNinja \
     ..
 ninja install
 popd
 popd
-rm -rf llvm-${LLVM_VERSION}.src.tar.xz llvm-${LLVM_VERSION}.src.tar llvm-${LLVM_VERSION}.src
-
 
 # clang is only used to precompile Gandiva bitcode
-if [ ${LLVM_VERSION_MAJOR} -lt 9 ]; then
+if [ "${LLVM_VERSION_MAJOR}" -lt 9 ]; then
   clang_package_name=cfe
 else
   clang_package_name=clang
 fi
-curl -sL https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/${clang_package_name}-${LLVM_VERSION}.src.tar.xz -o ${clang_package_name}-${LLVM_VERSION}.src.tar.xz
-unxz ${clang_package_name}-${LLVM_VERSION}.src.tar.xz
-tar xf ${clang_package_name}-${LLVM_VERSION}.src.tar
-pushd ${clang_package_name}-${LLVM_VERSION}.src
+
+pushd ${clang_package_name}
 mkdir build
 pushd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_MODULE_PATH="/llvm-project-${LLVM_VERSION}.src/cmake/Modules" \
     -DCLANG_INCLUDE_TESTS=OFF \
     -DCLANG_INCLUDE_DOCS=OFF \
     -DLLVM_INCLUDE_TESTS=OFF \
     -DLLVM_INCLUDE_DOCS=OFF \
-    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
+    -DLLVM_ENABLE_LIBEDIT=OFF \
+    -Wno-dev \
     -GNinja \
     ..
 ninja -w dupbuild=warn install # both clang and llvm builds generate llvm-config file
 popd
 popd
-rm -rf ${clang_package_name}-${LLVM_VERSION}.src.tar.xz ${clang_package_name}-${LLVM_VERSION}.src.tar ${clang_package_name}-${LLVM_VERSION}.src
+
+popd
+
+rm -rf llvm-project-${LLVM_VERSION}.src.tar.xz llvm-project-${LLVM_VERSION}.src.tar
