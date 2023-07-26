66a7c7f705a7ac7a2531a35d5e2f3754d8a0fe1d
Liam Sturge <liam.sturge@arm.com>
2023-07-26 13:38:44 +0100

    Pin Cython version to fix Cython compilation
    Cython 3.0.0 was recently released, but it is incompatible with the
    current .pxi definitions in python/tvm/_ffi/_cpython.

--- docker/install/ubuntu_install_python_package.sh
+++ docker/install/ubuntu_install_python_package.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-PYTHON_PACKAGES="six numpy pytest cython decorator scipy tornado typed_ast mypy \
+PYTHON_PACKAGES="six numpy pytest cython==0.29.34 decorator scipy tornado typed_ast mypy \
 orderedset antlr4-python3-runtime attrs requests Pillow packaging junitparser synr cloudpickle xgboost==1.5.0"
 
 # install libraries for python package on ubuntu
