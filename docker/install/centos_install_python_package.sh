eb08f29ec74f80781c8d228dcfd480537f557e41
Liam Sturge <liam.sturge@arm.com>
2023-08-03 11:26:26 +0100

    Pin Cython to fix compilation error
    Cython 3.0.0 was recently released, but it is incompatible with the
    current .pxi definitions in python/tvm/_ffi/_cpython.
    
    Cython has been pinned for TVM in PR: 15353 until a working fix is
    created. This patch replicates many of the changes for TLCPack
    installation, as we are seeing the same Cython errors when trying to use
    TLCPack Ubuntu Docker images.

--- docker/install/centos_install_python_package.sh
+++ docker/install/centos_install_python_package.sh
@@ -6,7 +6,7 @@ source /multibuild/manylinux_utils.sh
 
 eval "$(conda shell.bash hook)"
 
-PYTHON_PACKAGES="six numpy pytest cython decorator scipy tornado typed_ast mypy \
+PYTHON_PACKAGES="six numpy pytest cython==0.29.34 decorator scipy tornado typed_ast mypy \
 orderedset antlr4-python3-runtime attrs requests Pillow packaging junitparser synr cloudpickle xgboost==1.5.0"
 
 for env in $(conda env list | grep py | awk '{print $1}'); do
